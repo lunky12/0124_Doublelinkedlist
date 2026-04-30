07980f88016dbecf4ae722d9151ea9ebd15562f2
lunky12 <lunkyumy@anda.com>
2026-04-30 12:24:09 +0700

    cout

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -88,3 +88,9 @@ public:
             cout << "\nList is empty" << endl;
             return;
         }
+
+        cout << "\nEnter the roll number of the student whose record is to be deleted: ";
+        int rollNo;
+        cin >> rollNo;
+
+        Node *current = START;
