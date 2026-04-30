eea1af09771956cfefb1a56916921cf668fdbfbf
lunky12 <lunkyumy@anda.com>
2026-04-30 12:23:30 +0700

    menambahkan void (hapus)

--- Doublelinkedlist.cpp
+++ Doublelinkedlist.cpp
@@ -80,3 +80,11 @@ public:
 
         current->next = newNode; // Step 9d: current.next = newNode
     }
+
+    void hapus()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
